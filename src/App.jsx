58f89f35627a9785967bc922039e47220bad9ae9
NunezAN <alex101nunez@gmail.com>
2023-02-15 10:42:42 -0600

    completed counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="text-2xl text-red-500">hello</div>;
+  const [counter, setCounter] = useState(0);
+  function handleAddition() {
+    setCounter((current) => current + 1);
+  }
+  function handleSubtraction() {
+    setCounter((current) => current - 1);
+  }
+  return (
+    <div className="flex flex-row items-center justify-between max-w-sm mx-auto p-4">
+      <button
+        onClick={handleSubtraction}
+        className="p-2 bg-slate-400 rounded-xl"
+      >
+        -
+      </button>
+      <span className="text-3xl">{counter}</span>
+      <button onClick={handleAddition} className="p-2 bg-slate-400 rounded-xl">
+        +
+      </button>
+    </div>
+  );
 }
 
 export default App;
